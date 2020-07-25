a40689124f5100477120bdbda3570fbdf89847ac
Shahzeb Parkar <shahzebparkar@gmail.com>
2020-07-25 10:00:00 +0530

    Added Grid and style to the Component and main page

--- src/Components/pokedex.js
+++ src/Components/pokedex.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { AppBar, ToolBar,Grid,Card,CardContent } from '@material-ui/core'
+import { makeStyles} from '@material-ui/core/styles'
 
+const useStyles = makeStyles({
+  pokedexContainer:{
+    paddingTop:'20px',
+    paddingLeft:'50px',
+    paddingRight:'50px'
+  },
+}
+)
+
+const pokemonGrid =()=>{
+  return(
+   <Grid item xs={12} sm={4}>
+     <Card/>
+   </Grid> 
+  )
+}
 const Pokedex = () => {
-  return <div>This is PokeDex Page</div>;
+  const classes = useStyles();
+  return (
+    <>
+    <AppBar position='static'>
+      <ToolBar/>
+    </AppBar>
+    <Grid container spacing={4} className={classes.pokedexContainer}>
+      {
+        pokemonGrid()
+      }
+    </Grid>
+    </>
+    )
 };
 
 export default Pokedex;
